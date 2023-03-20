f3551fef50c66a3064af622cce7d20c2dd7a87d5
fangtexi <2395141263@qq.com>
2023-03-20 22:16:04 +0800

    创建网关全局过滤器

--- api-gateway/src/main/java/com/zsapi/gateway/CustomerGlobalFilter.java
+++ api-gateway/src/main/java/com/zsapi/gateway/CustomerGlobalFilter.java
@@ -1,12 +1,31 @@
 package com.zsapi.gateway;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
 /**
- * @Description TODO
+ * @Description 网关全局过滤器
  * @ClassName CustomerGlobalFilter
  * @Author 23951
  * @Date 2023/3/20 22:11
  * @Version 1.0
  */
+@Slf4j
+@Component
+public class CustomerGlobalFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("custom global filter");
+        return chain.filter(exchange);
+    }
 
-public class CustomerGlobalFilter {
+    @Override
+    public int getOrder() {
+        return -1;
+    }
 }
